477b237c5e15b25e95efd06b7706884726bfff3d
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-09-26 17:04:40 +0530

    style: hero-section

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { LinkField } from '@prismicio/client';
+import { PrismicNextLink } from '@prismicio/next';
+import clsx from 'clsx';
+
+type Props = {
+    buttonLink: LinkField;
+    buttonText: string | null;
+    className?: string;
+}
+
+export default function Button({ buttonLink, buttonText, className }: Props) {
+    return (
+        <PrismicNextLink className={clsx("rounded-xl bg-orange-600 px-5 py-4 text-center text-xl md:text-2xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700", className)} field={buttonLink}>
+            {buttonText}
+        </PrismicNextLink>
+    );
+}
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,9 @@
-import { Content } from "@prismicio/client";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { Bounded } from "@/components/Bounded";
+import Button from "@/components/Button";
+import { asText, Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { TextSplitter } from "./TextSplitter";
 
 /**
  * Props for `Hero`.
@@ -12,21 +15,39 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="hero"
     >
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicRichText field={slice.primary.subheading} />
-      <PrismicRichText field={slice.primary.body} />
-      <PrismicNextLink field={slice.primary.button_link}>
-        {slice.primary.button_text}
-      </PrismicNextLink>
-      <PrismicNextImage field={slice.primary.cans_image} />
-      <PrismicRichText field={slice.primary.second_heading} />
-      <PrismicRichText field={slice.primary.second_body} />
-      Placeholder component for hero (variation: {slice.variation}) Slices
-    </section>
+      <div className="grid">
+        <div className="grid h-screen place-items-center">
+          <div className="grid auto-rows-min place-items-center text-center">
+            <h1 className="hero-header mt-12 text-7xl md:text-[9rem] lg:text-[13rem] font-black uppercase leading-[.8] text-orange-500">
+              <TextSplitter text ={asText(slice.primary.heading)} wordDisplayStyle="block" className="hero-header-word" />
+            </h1>
+            <div className="hero-subheading mt-12 text-5xl lg:text-6xl font-semibold text-sky-950">
+              <PrismicRichText field={slice.primary.subheading} />
+            </div>
+            <div className="hero-body text-2xl font-normal text-sky-950">
+              <PrismicRichText field={slice.primary.body} />
+            </div>
+            <Button buttonLink={slice.primary.button_link} buttonText={slice.primary.button_text} className="hero-button mt-12" />
+          </div>
+        </div>
+        <div className="grid text-side relative z-[80] h-screen items-center gap-4 md:grid-cols-2">
+          <PrismicNextImage className="md:hidden w-full" field={slice.primary.cans_image} />
+          <div>
+            <h2 className="text-side-heading text-6xl lg:text-8xl text-balance uppercase font-black text-sky-950">
+              <TextSplitter text={asText(slice.primary.second_heading)} />
+            </h2>
+            <h2 className="tsxt-side-body mt-4 mx-w-xl text-balance text-xl font-normal text-sky-950">
+              <PrismicRichText field={slice.primary.second_body} />
+            </h2>
+          </div>
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
